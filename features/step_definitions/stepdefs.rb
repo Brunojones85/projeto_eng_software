be7633967829cf91ae6d3549dba1f9a78d0ed829
wagnermarques <wagnerdocri@gmail.com>
2018-08-19 00:07:01 -0300

    fiz um rspec --init que criou .rspec e ./spec mas ainda não escrevi nada de rspec. Usei https://docs.cucumber.io/guides/10-minute-tutorial/ pra criar um cenário de teste passando com cucumber que ta dentro de ./features. Pra rodar o teste com cucumber e so dar o comando cucumber

---
+++ features/step_definitions/stepdefs.rb
@@ -0,0 +1,19 @@
+module FridayStepHelper
+  def is_it_friday?(day)
+    "AindaNao"
+  end
+end
+World FridayStepHelper
+
+Given("today is Sunday") do
+  @today = 'Sunday'
+end
+
+When("I ask whether it's Friday yet") do
+  @actual_answer = is_it_friday?(@today)
+  puts @actual_answer 
+end
+
+Then("I should be told {string}") do |expected_answer|
+  expect(@actual_answer).to eq(expected_answer)
+end
