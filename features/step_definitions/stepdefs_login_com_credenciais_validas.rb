1a86045a5a16c443d1778f262738036640fe64e7
wagnermarques <wagnerdocri@gmail.com>
2018-09-05 01:18:53 -0300

    rodei o db:migrate que gerou o arquivo db/schema.rb porque a execucao do rake (que por sua vez roda o rake spec e o cucumber em sequencia) exigiu. Para os testes funcionarem tem que criar o banco novo_sus_test, criar o usuario admin com a senha admin123 e transformar ele em administrador com alter user admin with superuser, ver esses ajustes em config/database.yml. Escrevi testes funcionais para o requisito funcional de login

---
+++ features/step_definitions/stepdefs_login_com_credenciais_validas.rb
@@ -0,0 +1,24 @@
+Given("Convidado encontra na tela de login") do
+  pending # Write code here that turns the phrase above into concrete actions
+end
+
+When("O convidado preenche {string} with {string}") do |string, string2|
+  pending # Write code here that turns the phrase above into concrete actions
+end
+
+When("preenche {string} with {string}") do |string, string2|
+  pending # Write code here that turns the phrase above into concrete actions
+end
+
+When("pressiona o botao de {string}") do |string|
+  pending # Write code here that turns the phrase above into concrete actions
+end
+
+Then("o nome dele deve constar na pagina como usuario logado na pagina principal") do
+  pending # Write code here that turns the phrase above into concrete actions
+end
+
+Then("tambem deve ver a mensagem {string}") do |string, table|
+  # table is a Cucumber::MultilineArgument::DataTable
+  pending # Write code here that turns the phrase above into concrete actions
+end
