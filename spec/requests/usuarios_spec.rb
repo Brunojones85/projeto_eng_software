d04e394ae9ec9a62155b55a3dea175584d9621af
migvasc <miguel.vasconcelos@usp.br>
2018-09-08 23:34:29 -0300

    Revert "Ajuste spec"
    This reverts commit 4f1f944d6a1a98f559e4dfa84cc2d53d6aa99f6b.

--- spec/requests/usuarios_spec.rb
+++ spec/requests/usuarios_spec.rb
@@ -2,7 +2,7 @@ require 'rails_helper'
 
 RSpec.describe "Usuarios", type: :request do
   describe "GET /usuarios" do
-    it "works! (now write some real specs)" do
+    it "return user with succes" do
       get usuarios_path
       expect(response).to have_http_status(200)
     end
