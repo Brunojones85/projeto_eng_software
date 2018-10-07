c4483b32c3fc5cc855f8adf4961f11d54fed0b09
migvasc <miguel.vasconcelos@usp.br>
2018-10-07 20:35:05 -0300

    testes para melhorar nota no codeclimate

--- features/step_definitions/cadastro_de_medico_steps.rb
+++ features/step_definitions/cadastro_de_medico_steps.rb
@@ -1,44 +1,5 @@
 require_relative "steps_helper"
 
-#Ver ser util para caso de teste de registrar medico
-def sign_up_com_dados_validos
-  exclui_usuario
-  visit '/usuarios/sign_up'
-  fill_in "usuario_nome", :with => @convidado[:nome]
-  fill_in "usuario_numerosus", :with => @convidado[:numerosus]
-  fill_in "usuario_cpf", :with => @convidado[:cpf]
-
-  #fix-me:
-  #seria melhor mapear 'option[9]' para o atributo  @convidado[:dtanasc_mes]
-  #seria melhor que apenas selecionar a opcao 9, mas a opcao nove ainda e valida
-  #pra efeitos desse caso de teste
-  #find("#usuario_dtanasc_3i").find(:xpath, 'option['+@convidado[:dtanasc_dia]+']').select_option
-  #find("#usuario_dtanasc_2i").find(:xpath, 'option[9]').select_option
-
-  #fix-me: idem acima com parametro @convidado[:dtanasc_ano]
-  #find("#usuario_dtanasc_1i").find(:xpath, 'option[3]').select_option
-
-  fill_in "usuario_telfixo", :with => @convidado[:telfixo]
-  fill_in "usuario_telcel", :with => @convidado[:telcel]
-  fill_in "usuario_sexo", :with => @convidado[:sexo]
-
-  fill_in "usuario_email", :with => @convidado[:email]
-  fill_in "usuario_password", :with => @convidado[:password]
-  fill_in "usuario_password_confirmation", :with => @convidado[:password_confirmation]
-
-  find(:xpath,"/html/body/div/div/form/div[11]/input").click
-
-  find_user
-end
-
-def sign_up_sem_informar_nenhum_dado_no_form
-  exclui_usuario
-  visit '/usuarios/sign_up'
-  find(:xpath,"/html/body/div/div/form/div[11]/input").click
-end
-
-
-
 Given("Eu sou o Administrador") do
   expect(1==1)
 end
--- features/step_definitions/steps_helper.rb
+++ features/step_definitions/steps_helper.rb
@@ -48,3 +48,77 @@ def fazer_login
   fill_in "usuario_password", :with => @convidado[:password]
   find(:xpath, "/html/body/div/div/form/button").click
 end
+
+def sign_up_com_dados_validos
+  exclui_usuario
+  visit '/usuarios/sign_up'
+  fill_in "usuario_nome", :with => @convidado[:nome]
+  fill_in "usuario_numerosus", :with => @convidado[:numerosus]
+  fill_in "usuario_cpf", :with => @convidado[:cpf]
+
+  #fix-me:
+  #seria melhor mapear 'option[9]' para o atributo  @convidado[:dtanasc_mes]
+  #seria melhor que apenas selecionar a opcao 9, mas a opcao nove ainda e valida
+  #pra efeitos desse caso de teste
+  #find("#usuario_dtanasc_3i").find(:xpath, 'option['+@convidado[:dtanasc_dia]+']').select_option
+  #find("#usuario_dtanasc_2i").find(:xpath, 'option[9]').select_option
+
+  #fix-me: idem acima com parametro @convidado[:dtanasc_ano]
+  #find("#usuario_dtanasc_1i").find(:xpath, 'option[3]').select_option
+
+  fill_in "usuario_telfixo", :with => @convidado[:telfixo]
+  fill_in "usuario_telcel", :with => @convidado[:telcel]
+  fill_in "usuario_sexo", :with => @convidado[:sexo]
+
+  fill_in "usuario_email", :with => @convidado[:email]
+  fill_in "usuario_password", :with => @convidado[:password]
+  fill_in "usuario_password_confirmation", :with => @convidado[:password_confirmation]
+
+  find(:xpath,"/html/body/div/div/form/div[11]/input").click
+
+  find_user
+end
+
+def sign_up_sem_informar_nenhum_dado_no_form
+  exclui_usuario
+  visit '/usuarios/sign_up'
+  find(:xpath,"/html/body/div/div/form/div[11]/input").click
+end
+
+def preencherCamposLocal(objeto)
+  fill_in "local_nome", :with => objeto[:nome]
+  fill_in "local_cep", :with => objeto[:cep]
+  fill_in "local_rua", :with =>objeto[:rua]
+  fill_in "local_estado", :with => objeto[:estado]
+  fill_in "local_cidade", :with => objeto[:cidade]
+  fill_in "local_bairro", :with => objeto[:bairro]
+  fill_in "local_numero", :with => objeto[:numero]
+  fill_in "local_telefone", :with => objeto[:telefone]
+end
+
+
+def cria_local_valido
+  @localvalido = {
+    :nome => "Hospital X",
+    :cep => Faker::Address.zip_code,
+    :rua => Faker::Address.street_name,
+    :numero => Faker::Address.building_number,
+    :estado => Faker::Address.state_abbr,
+    :cidade => Faker::Address.city ,
+    :bairro => Faker::Address.community,
+    :telefone => Faker::PhoneNumber.phone_number
+  }
+end
+
+def cria_local_invalido
+  @localinvalido = {
+    :nome => nil,
+    :cep => nil,
+    :rua => nil,
+    :numero => nil,
+    :estado => nil,
+    :cidade => nil,
+    :bairro => nil,
+    :telefone => nil
+  }
+end
