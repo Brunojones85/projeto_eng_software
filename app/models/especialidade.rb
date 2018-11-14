2b958a3c2af02fa36e7d391b6bcc6dc0762595d4
osboxes.org <sabariscarballo@gmail.com>
2018-11-13 23:03:37 -0500

    add arquivos e cometários do rdoc

--- app/models/especialidade.rb
+++ app/models/especialidade.rb
@@ -1,6 +1,12 @@
+# -------------------------------------------------
+#Esta classe representa o cadastro de especialiades 
+#--------------------------------------------------
 class Especialidade < ActiveRecord::Base
   has_and_belongs_to_many :medicos
   validates_presence_of :Nome, :message => "- Campo obrigatorio"
+   #Campo campo ativo foi criado para não excluir o rgistro especialiade 
+   # por ter relacionamento com outras tabelas e dados históricos de consulta 
+   # apenas desativamos o registro
    validates :ativo, inclusion: { in: [ true, false ] }      
 end
 
